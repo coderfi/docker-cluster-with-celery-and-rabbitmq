cafee8a64e80eac8f79a827af8e551bf1ef83f92
Fairiz Azizi <coderfi@gmail.com>
2017-05-07 21:18:33 -0700

    use host networking

--- test_celery/celery.py
+++ test_celery/celery.py
@@ -1,4 +1,4 @@
 from __future__ import absolute_import
 from celery import Celery
-app = Celery('test_celery',broker='amqp://admin:mypass@10.211.55.12:5672',backend='rpc://',include=['test_celery.tasks'])
+app = Celery('test_celery',broker='amqp://guest@192.168.1.6:5672',backend='rpc://',include=['test_celery.tasks'])
 
